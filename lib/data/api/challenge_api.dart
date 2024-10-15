16bf87b16c2a2779cd0249a9b9fce4310d22f048
KyungaIM <avocadopajamas@gmail.com>
2024-10-15 23:08:30 +0900

    데이터 처리 구조 생성

---
+++ lib/data/api/challenge_api.dart
@@ -0,0 +1,6 @@
+import 'package:dailystep/data/api/result/api_error.dart';
+import 'package:dailystep/data/challenge_repository.dart';
+
+class ChallengeApi implements ChallengeRepository<ApiError> {
+
+}
