cf5aefa9c987063c583336d5d180a1bd500494af
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2024-10-24 23:20:59 +0900

    에러처리

--- lib/data/api/result/api_error.dart
+++ lib/data/api/result/api_error.dart
@@ -13,18 +13,41 @@ class ApiError {
     required this.message,
   });
 
-  static createErrorResult(e) {
+  static final Map<int, String> _errorMessage = {
+    200: '성공적으로 처리 되었습니다.',
+    201: '성공적으로 생성 되었습니다.',
+    301: '요청한 리소스가 다른 위치로 이동했습니다.',
+    401: '인증이 필요합니다. 로그인을 확인해 주세요.',
+    403: '접근이 거부되었습니다. 권한을 확인해 주세요.',
+    404: '요청한 리소스를 찾을 수 없습니다.',
+    405: '지원되지 않는 HTTP 메소드입니다.',
+    500: '서버 내부 오류가 발생했습니다. 나중에 다시 시도해주세요.',
+    501: '요청된 기능이 서버에 구현되어 있지 않습니다.',
+    504: '서버 응답이 지연되고 있습니다. 잠시 후 다시 시도해주세요.',
+    505: '지원되지 않는 HTTP 프로토콜 버전입니다.',
+  };
+
+  static SimpleResult<ApiError, dynamic> createErrorResult(dynamic e) {
     if (e is DioException) {
-      if (!kReleaseMode) {
-        return SimpleResult.failure(ApiError(
-            message: e.error?.toString() ?? e.message ?? 'message is empty',
-            isApplicationError: e.response == null));
-      } else {
-        return SimpleResult.failure(
-            ApiError(message: 'api_error', statusCode: e.response?.statusCode));
-      }
+      final response = e.response;
+      final statusCode = response?.statusCode;
+      final errorMessage = _errorMessage[statusCode] ?? '알 수 없는 오류가 발생했습니다.';
+
+      final apiError = ApiError(
+        message: errorMessage,
+        statusCode: statusCode,
+        isApplicationError: response == null,
+      );
+
+      _logError(apiError);
+
+      return SimpleResult.failure(apiError);
     }
 
     return SimpleResult.failure(ApiError(message: 'api_error'));
+  }
+
+  static void _logError(ApiError error) {
+    print('Error: ${error.message} (Status code: ${error.statusCode})');
   }
 }
--- lib/data/api/result/simple_result.dart
+++ lib/data/api/result/simple_result.dart
@@ -2,111 +2,98 @@ import 'dart:async';
 
 class SimpleResult<SuccessType, FailureType> {
   bool get isFailure => this is _Failure<SuccessType, FailureType>;
-
   bool get isSuccess => this is _Success<SuccessType, FailureType>;
 
   FailureType get failureData {
     if (isFailure) {
       final data = (this as _Failure<SuccessType, FailureType>).data;
       if (data == null) {
-        throw const FormatException('Failure data not exist');
+        throw DataNotExistException('Failure data not exist');
       }
       return data;
     }
-
-    throw const FormatException('This is not Failure type');
+    throw InvalidTypeException('This is not a Failure type');
   }
 
   SuccessType get successData {
     if (isSuccess) {
       final data = (this as _Success<SuccessType, FailureType>).data;
       if (data == null) {
-        throw const FormatException('Success data not exist');
+        throw DataNotExistException('Success data not exist');
       }
       return data;
     }
-
-    throw const FormatException('This is not Success type');
+    throw InvalidTypeException('This is not a Success type');
   }
 
-  SimpleResult runIfSuccess(FutureOr<void> Function(SuccessType data) function) {
+  FailureType? get nullableFailureData => isFailure ? (this as _Failure<SuccessType, FailureType>).data : null;
+  SuccessType? get nullableSuccessData => isSuccess ? (this as _Success<SuccessType, FailureType>).data : null;
+
+  SimpleResult<SuccessType, FailureType> runIfSuccess(
+      FutureOr<void> Function(SuccessType? data) function) {
     _runSuccess(function);
     return this;
   }
 
-  SimpleResult runIfFailure(FutureOr<void> Function(FailureType error) function) {
+  SimpleResult<SuccessType, FailureType> runIfFailure(
+      FutureOr<void> Function(FailureType? error) function) {
     _runFailure(function);
     return this;
   }
 
-  FutureOr<SimpleResult> runIfSuccessAsync(
-      FutureOr<void> Function(SuccessType data) function) async {
+  Future<SimpleResult<SuccessType, FailureType>> runIfSuccessAsync(
+      FutureOr<void> Function(SuccessType? data) function) async {
     await _runSuccess(function);
     return this;
   }
 
-
-  FutureOr<SimpleResult> runIfFailureAsync(
-      FutureOr<void> Function(FailureType error) function) async {
+  Future<SimpleResult<SuccessType, FailureType>> runIfFailureAsync(
+      FutureOr<void> Function(FailureType? error) function) async {
     await _runFailure(function);
     return this;
   }
 
-  bool get _isSuccessDataExist {
-    if ((this as _Success<SuccessType, FailureType>).data == null) {
-      return false;
-    }
-    return true;
-  }
-
-  bool get _isFailureDataExist {
-    if ((this as _Failure<SuccessType, FailureType>).data == null) {
-      return false;
-    }
-    return true;
-  }
-
-  Future<void> _runSuccess(FutureOr<void> Function(SuccessType data) function) async {
+  Future<void> _runSuccess(FutureOr<void> Function(SuccessType? data) function) async {
     if (isSuccess) {
-      if (!_isSuccessDataExist) {
-        await function(null as SuccessType);
-      } else {
-        await function(successData);
-      }
+      await function(nullableSuccessData);
     }
   }
 
-
-  Future<void> _runFailure(FutureOr<void> Function(FailureType error) function) async {
+  Future<void> _runFailure(FutureOr<void> Function(FailureType? error) function) async {
     if (isFailure) {
-      if (!_isFailureDataExist) {
-        await function(null as FailureType);
-      } else {
-        await function(failureData);
-      }
+      await function(nullableFailureData);
     }
   }
 
-
-  // ignore: library_private_types_in_public_api
-  static _Failure<SuccessType, FailureType> failure<SuccessType, FailureType>([FailureType? data]) {
+  static _Failure<SuccessType, FailureType> failure<SuccessType, FailureType>(
+      [FailureType? data]) {
     return _Failure<SuccessType, FailureType>(data);
   }
 
-  // ignore: library_private_types_in_public_api
-  static _Success<SuccessType, FailureType> success<SuccessType, FailureType>([SuccessType? data]) {
+  static _Success<SuccessType, FailureType> success<SuccessType, FailureType>(
+      [SuccessType? data]) {
     return _Success<SuccessType, FailureType>(data);
   }
 }
 
-class _Success<SuccessType, FailureType> extends SimpleResult<SuccessType, FailureType> {
+class _Success<SuccessType, FailureType>
+    extends SimpleResult<SuccessType, FailureType> {
   final SuccessType? data;
-
   _Success(this.data);
 }
 
-class _Failure<SuccessType, FailureType> extends SimpleResult<SuccessType, FailureType> {
+class _Failure<SuccessType, FailureType>
+    extends SimpleResult<SuccessType, FailureType> {
   final FailureType? data;
-
   _Failure(this.data);
+}
+
+class DataNotExistException implements Exception {
+  final String message;
+  DataNotExistException(this.message);
+}
+
+class InvalidTypeException implements Exception {
+  final String message;
+  InvalidTypeException(this.message);
 }
