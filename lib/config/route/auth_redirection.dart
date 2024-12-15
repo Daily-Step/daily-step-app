2fb4bd581708043f50dbd00b67c2f960e2b3bd99
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2024-12-15 11:02:17 +0900

    fix/kakao_login(임시)

--- lib/config/route/auth_redirection.dart
+++ lib/config/route/auth_redirection.dart
@@ -1,9 +1,11 @@
 import 'package:dailystep/feature/auth/social_type.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../data/api/login_api.dart';
 import '../../feature/auth/social_login_repository.dart';
+import '../../feature/auth/viewmodel/login_viewmodel.dart';
 import '../../model/user/user_model.dart';
 
 class DailyStepAuth extends ChangeNotifier {
@@ -43,12 +45,12 @@ class DailyStepAuth extends ChangeNotifier {
 
   Future<void> signUp(BuildContext context) async {
     _signedUp = true;
+    _signedIn = true;
     notifyListeners();
     GoRouter.of(context).go('/main/home');
   }
 
   Future<void> signIn(BuildContext context) async {
-    user = await loginApi.me();
     _signedIn = true;
     notifyListeners();
 
@@ -61,20 +63,26 @@ class DailyStepAuth extends ChangeNotifier {
     }
   }
 
-  String? guard(BuildContext context, GoRouterState state) {
-    final bool signedIn = this.signedIn;
-    final bool signedUp = this.signedUp;
-    final location = state.uri.path;
+  String? guard(GoRouterState state, bool isLoggedIn) {
+    final isLoginPage = state.matchedLocation == '/signIn' || state.matchedLocation == '/signUp';
+    final isSignupPage = state.matchedLocation == '/signUp';
 
-    if (location == '/signIn' || location == '/signUp') {
-      return null;
+    if (isLoggedIn) {
+      // 로그인 상태에서 로그인 또는 가입 페이지에 접근하면 홈으로 리디렉션
+      if (isLoginPage || state.matchedLocation == '/') {
+        return '/main/home';
+      }
+      if (isSignupPage) {
+        return '/main/home';
+      }
     }
-    if (!signedIn) {
+
+    // 로그인 안 된 상태에서 로그인 페이지 외 다른 페이지 접근 시 로그인 페이지로 리디렉션
+    if (!isLoggedIn && state.matchedLocation != '/signIn' && state.matchedLocation != '/signUp') {
       return '/signIn';
-    } else if (signedIn && !signedUp) {
-      return '/signUp';
     }
-    return null;
+
+    return null;  // 리디렉션 없이 그대로 페이지 유지
   }
 }
 
