4a8e750363b9d066eb5f38e2b2674cce816b45d6
KyungaIM <avocadopajamas@gmail.com>
2025-01-18 14:14:02 +0900

    상세페이지ui overflow버그 수정

--- lib/widgets/widget_card.dart
+++ lib/widgets/widget_card.dart
@@ -1,4 +1,3 @@
-import 'package:dailystep/common/util/size_util.dart';
 import 'package:dailystep/widgets/widget_constant.dart';
 import 'package:flutter/material.dart';
 
@@ -14,19 +13,16 @@ class WCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 8.0 * su, right: 8.0 * su),
-      child: Card(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: globalBorderRadius,
-        ),
-        elevation: 1,
-        shadowColor: Color(0xD5E7E7E7),
-        child: Padding(
-          padding: EdgeInsets.all(padding ?? 16.0),
-          child: child,
-        ),
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: globalBorderRadius,
+      ),
+      elevation: 1,
+      shadowColor: Color(0xD5E7E7E7),
+      child: Padding(
+        padding: EdgeInsets.all(padding ?? 16.0),
+        child: child,
       ),
     );
   }
